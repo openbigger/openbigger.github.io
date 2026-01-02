89d4ddf98a948a037fb188cec120e8bb3ce78d04
Yuri <yuri@Exdyna-Mac-Studio.local>
2026-01-02 15:58:54 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./**/*.html"], // 极其明确地指向 index.html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
