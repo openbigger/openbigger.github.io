7c081dff4e31b4014c8d306a18ce4245812aab62
Yuri <yuri@Exdyna-Mac-Studio.local>
2026-01-02 17:08:39 +0800

    去掉tailwind config，添加一张图片

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./**/*.html"], // 极其明确地指向 index.html
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
